a1420b9f700f0862fe28dcdfeef35fe5ff875d6f
Programistich <alexasda051@gmail.com>
2021-05-12 20:25:47 +0300

    DataClass errors

---
+++ src/utils/ErrorMsg.java
@@ -0,0 +1,13 @@
+package utils;
+
+public class ErrorMsg {
+
+    public boolean isError;
+    public String errorMsg;
+
+    public ErrorMsg(boolean isError, String errorMsg) {
+        this.isError = isError;
+        this.errorMsg = errorMsg;
+    }
+
+}
